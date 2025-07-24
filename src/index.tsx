5c675ee533671ea076053b54c83c2c45450632a6
gekronik <sergeybv22@gmail.com>
2025-07-24 20:43:32 +0300

    6 Роутинг Code splitting Lazy Suspence

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import Counter from "./components/Counter";
 import {render} from "react-dom";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
 
 render(
-    <div>
+    <BrowserRouter>
         <App />
-    </div>,
+    </BrowserRouter>,
     document.getElementById('root')
 )
