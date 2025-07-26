791378b7cd236a9eccdca90484456ca112a52e3b
gekronik <sergeybv22@gmail.com>
2025-07-26 15:29:40 +0300

    7 Организация стилей. Добавляем темы

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import Counter from "./components/Counter";
 import {render} from "react-dom";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 
 render(
     <BrowserRouter>
+        <ThemeProvider>
         <App />
+        </ThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
 )
